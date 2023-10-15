62c1e584a9ab1409d3202acd07d961bb031112a3
ntxinh <nguyentrucxjnh@gmail.com>
2023-10-15 20:03:20 +0700

    :star: Build 2 include passwall 1 and 2

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,21 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Cancel bootstrap as default theme
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci-nginx/Makefile
+
+# Add author information
+#sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='xxxWrt-$(date +%Y%m%d)'/g" package/lean/default-settings/files/zzz-default-settings
+#sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' xxx'/g" package/lean/default-settings/files/zzz-default-settings
+
+# Change Hostname
+# sed -i "s/hostname='.*'/hostname='xxxWrt'/g" package/base-files/files/bin/config_generate
+
+# Fan control
+# curl -sfL https://raw.githubusercontent.com/friendlyarm/friendlywrt/master-v21.02/target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol.sh --create-dirs -o files/usr/bin/fa-fancontrol.sh
+# curl -sfL https://raw.githubusercontent.com/friendlyarm/friendlywrt/master-v21.02/target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol-direct.sh --create-dirs -o files/usr/bin/fa-fancontrol-direct.sh
+# curl -sfL https://raw.githubusercontent.com/friendlyarm/friendlywrt/master-v21.02/target/linux/rockchip/armv8/base-files/etc/init.d/fa-fancontrol --create-dirs -o files/etc/init.d/fa-fancontrol
+# chmod +x files/usr/bin/fa-*.sh files/etc/init.d/fa-fancontrol
